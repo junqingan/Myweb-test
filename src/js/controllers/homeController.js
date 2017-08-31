3ebb8182bdc03687f7629fe9cea191fee0897545
junqingan <junqingan@pptv.com>
2017-08-31 18:48:45 +0800

    commit

--- src/js/controllers/homeController.js
+++ src/js/controllers/homeController.js
@@ -1,8 +1,9 @@
 var Nav = require("../module/nva/nav.js"),
     banner = require("../module/bannersilder/slider"),
     goToTop = require("../module/gotop/gotop");
+    citylist = require("../module/citylist/citylist");
 
-var Home = function () {
+var Home = function() {
     this.options = {};
     this.redianM = null, this.redianC = null;
 };
@@ -11,23 +12,24 @@ var Home = function () {
  * 初始化
  * @param options Object 参数传递
  */
-Home.prototype.init = function (options) {
+Home.prototype.init = function(options) {
     $.extend(this.options, options);
     this.initNav();
     this.initbanner();
     this.initgoToTop();
+    this.initcitylist();
 }
 
 /**
  * 导航动态点击
  */
-Home.prototype.initNav = function () {
+Home.prototype.initNav = function() {
     Nav.init();
 };
 /**
  * banner
  */
-Home.prototype.initbanner = function () {
+Home.prototype.initbanner = function() {
     banner.init();
 };
 /**
@@ -39,11 +41,17 @@ Home.prototype.initgoToTop = function() {
         time: 500
     });
 };
+/**
+ * citylist
+ */
+Home.prototype.initcitylist = function() {
+    citylist.init();
+};
 
 var instance;
 
 module.exports = {
-    'getInstance': function (options) {
+    'getInstance': function(options) {
         return instance || (instance = new Home(options));
     }
 };
