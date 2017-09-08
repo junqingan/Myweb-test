6d1b7d699cde17924e8e34c035b6e95a25d82fd5
junqingan <junqingan@pptv.com>
2017-09-08 15:18:37 +0800

    commit

--- src/js/module/product/product.js
+++ src/js/module/product/product.js
@@ -3,9 +3,71 @@ function start() {
         product_list = $('#product-list .con');
 
     product_menu.click(function (e) {
+
         $(this).addClass('on').siblings().removeClass('on');
-        product_list.eq($(this).index()).stop().fadeIn(1000).addClass('current').siblings().stop().fadeOut(1000).removeClass('current');
+
+        product_list.eq($(this).index()).stop().addClass('current').siblings().stop().removeClass('current');
     });
+
+
+    $(function () {
+        $.getJSON("../../../json/pro-list.json", function (data) {
+            console.log(data);
+            var cHtml = "";
+            for (var i = 0; i < data.length; i++) {
+                cHtml +=
+                    '<li>' +
+                    '<a href="javascript:;">' +
+                    '<p class="txt">' + data[i].name + '</p>' +
+                    '<img class="food_pic" src=' + data[i].img_sm + '>' +
+                    '</a>' +
+                    '</li>';
+            }
+            $(".hoslist").html(cHtml); //将数据增加到页面中
+            $("div.holder").jPages({
+                containerID: "itemContainer ",
+                previous: "上一页",
+                next: "下一页",
+                perPage: 16
+            })
+        })
+    });
+        // 寿司单品渲染
+    $(function () {
+        $.getJSON("../../../json/pro-shousilist.json", function (data) {
+            console.log(data);
+            var cHtml = "";
+            for (var i = 0; i < data.length; i++) {
+                cHtml +=
+                    '<li>' +
+                    '<a href="javascript:;">' +
+                    '<p class="txt">' + data[i].name + '</p>' +
+                    '<img class="food_pic" src=' + data[i].img_sm + '>' +
+                    '</a>' +
+                    '</li>';
+            }
+            $("#shousilist").html(cHtml); //将数据增加到页面中
+        })
+    });
+
+    // 酒水列表渲染
+    $(function () {
+        $.getJSON("../../../json/pro-jiushui.json", function (data) {
+            console.log(data);
+            var cHtml = "";
+            for (var i = 0; i < data.length; i++) {
+                cHtml +=
+                    '<li>' +
+                    '<a href="javascript:;">' +
+                    '<p class="txt">' + data[i].name + '</p>' +
+                    '<img class="food_pic" src=' + data[i].img_sm + '>' +
+                    '</a>' +
+                    '</li>';
+            }
+            $("#jiushui").html(cHtml); //将数据增加到页面中
+        })
+    })
+
 }
 
 function init() {
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,6 +4,7 @@
 window.$ = require('jquery');
 require('../utils/SuperSlide');
 require('../utils/Flipster');
+require('../utils/paged');
 var homeController = require("../controllers/homeController").getInstance();
 var indexLess = require('../../css/index.min.less');
 
