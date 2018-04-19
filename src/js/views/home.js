b64c7d5ab66948bee87363d8956b642a0b79420e
anjunqing <738418625@qq.com>
2018-04-19 21:25:08 +0800

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,11 +1,8 @@
-/**
- * 
- */
 window.$ = require('jquery');
 require('../utils/SuperSlide');
 require('../utils/Flipster');
 require('../utils/paged');
-var homeController = require("../controllers/homeController").getInstance();
+var homeController = require("../controllers/homeController.js").getInstance();
 var indexLess = require('../../css/index.min.less');
 
 homeController.init();
